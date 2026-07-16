62372a59ab0c2d87764337af3f4194a41f1a62d3
BATMINJI <minjiconan@naver.com>
2026-07-16 13:37:18 +0900

    feat : Animator 추가, playerMoving 추가, bool 파라미터 연동

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator anim;
     private Rigidbody2D rb;
 
     private float xInput;
@@ -11,17 +13,26 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();       // GetComponentInChildren : 자식 오브젝트에 있는 컴포넌트도 가져올 수 있음
     }
 
     private void Update()
     {
         HandleInput();
         HandleMovement();
+        HandleAnimations();
+    }
+
+    private void HandleAnimations()
+    {
+        bool isMoving = rb.linearVelocityX != 0;        // linearVelocityX : Rigidbody2D의 수평 속도
+
+        anim.SetBool("isMoving", isMoving);             // SetBool : Animator Controller의 파라미터를 설정
     }
 
     private void HandleInput()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
+        xInput = Input.GetAxisRaw("Horizontal");        // GetAxisRaw : 입력값을 -1, 0, 1로 반환 (왼쪽, 없음, 오른쪽)
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
