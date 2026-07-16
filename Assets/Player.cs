9ef7cf54f9e29e2b6f67beba12f5a1527283ec24
BATMINJI <minjiconan@naver.com>
2026-07-16 15:30:06 +0900

    feat : player 점프 중복 X, 바닥으로 레이캐스트, 바닥 레이어 추가

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,11 +6,17 @@ public class Player : MonoBehaviour
     private Animator anim;
     private Rigidbody2D rb;
 
+    [Header("Player Movement Settings")]
     private float xInput;
     [SerializeField] private float moveSpeed = 3.5f;     // SerializeField : private 필드에 인스펙터에서 접근 가능하도록 허용
     [SerializeField] private float jumpForce = 8.0f;
+    private bool facingRight = true;
 
-    [SerializeField] private bool facingRight = true;
+    [Header("Collision Settings")]
+    [SerializeField] private float groundCheckDistance = 0.1f;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded;
+
 
     private void Awake()
     {
@@ -20,6 +26,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleCollision();
         HandleInput();
         HandleMovement();
         HandleAnimations();
@@ -50,7 +57,17 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+        if(isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+        }
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+        // Raycast : 레이저를 쏘아서 충돌 여부를 확인하는 함수
+        // true : 충돌, false : 충돌하지 않음
     }
 
     private void HandleFlip()
@@ -68,5 +85,10 @@ public class Player : MonoBehaviour
     {
         transform.Rotate(0, 180, 0);
         facingRight = !facingRight;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -groundCheckDistance, 0));    
     }
 }
