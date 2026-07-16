56af18fc6c8b15d68bce1fc98db3730ad883e9a2
BATMINJI <minjiconan@naver.com>
2026-07-16 11:31:50 +0900

    [Practice] 접근 제어, Player Input X 움직임

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public string playerName = "Minji";
-    public int age = 24;
-    public float moveSpeed = 2.5f;
-    public bool gameOver = false;
+    private Rigidbody2D rb;
 
-    public Rigidbody2D rb;
+    private float xInput;
+    [SerializeField] private float moveSpeed = 3.5f;     // SerializeField : private 필드에 인스펙터에서 접근 가능하도록 허용
 
-    private void Start()
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // xInput = Input.GetAxis("Horizontal");        // -1 ~ 1 사이의 값 반환
+        xInput = Input.GetAxisRaw("Horizontal");        // -1 또는 1로만 반환
+
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
     }
 }
